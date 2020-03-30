8562cc7259fd817025636586b694865f50658b6b
Benjamin Wichser <benwichser@gmail.com>
2020-03-30 20:42:41 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
 # Your code goes here!
+
+class Anagram 
+
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(matches)
+    matches.reject!{|word| word.length != @word.length}
+    confirmed_matches = []
+    matches.each do |word|
+        if word.split("").sort == @word.split("").sort
+          confirmed_matches << word
+        end
+    end 
+
+    confirmed_matches 
+  end
+    
+    
+  
+end 
