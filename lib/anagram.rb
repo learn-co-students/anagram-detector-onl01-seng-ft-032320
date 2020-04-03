e58a3ca8ba84fd0e690de8190437bd4c6273bbb5
Christopher Reinken <christopherreinken@gmail.com>
2020-04-03 15:45:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,23 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :anagram_test
+  
+  def initialize(word)
+    @anagram_test = word
+  end
+  
+  def match(test_array)
+    return_array = []
+    test_array.each do |test_word|
+      a = test_word.split("")
+      b = @anagram_test.split("")
+      
+      if a.sort == b.sort
+        return_array << a.join
+      end
+    end
+    
+    return_array
+  end
+  
+end
