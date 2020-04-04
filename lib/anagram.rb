420b1d567b72640f8b88423e085cd6541942186d
Javante Nelson <javantenelson@gmail.com>
2020-04-04 01:04:11 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word, :name
+
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+  
+  array.select do |wordy| wordy.split("").sort == @word.split("").sort
+  
+    end
+  end
+end
