6c3f84b01dce0f179a9acce28a4908aafe22e0fb
Nashmeyah Al-Rekabi <nashalrekabi@gmail.com>
2020-04-04 18:14:26 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word #a similar word consisting of the same letters
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words_arr)
+    words_arr.select do |word|
+      word.split("").sort == @word.split("").sort
+    end
+  end
+end
+
+#takes a list of anagrams
+#split the list
+#itterate through new array of possible anagrams
+#match word with list of anagram that returns true or false
