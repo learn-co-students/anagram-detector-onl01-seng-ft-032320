0b0004822366362644cbf38d9b1eaa0421e12e3d
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-05 06:57:01 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(words)
+    matches = []
+    
+    words.each do |a_word|
+      if a_word.split("").sort == @word.split("").sort
+        matches << a_word
+      end
+    end
+    matches
+  end
+  
+end
